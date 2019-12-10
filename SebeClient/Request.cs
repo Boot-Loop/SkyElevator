bd6aa4b2124a5d88cbd3f04c296802cc0caf0271
ThakeeNathees <thakeenathees@gmail.com>
2019-12-10 23:20:40 +0530

    :cookie: response cookie created :cookie:

--- SebeClient/Request.cs
+++ SebeClient/Request.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SebeClient
 {
 	public class Request
 	{
+		public static async Task CookeiTest()
+		{
+			CookieContainer cookies = new CookieContainer();
+			HttpClientHandler handler = new HttpClientHandler();
+			handler.CookieContainer = cookies;
+
+			HttpClient client = new HttpClient(handler);
+			Uri uri = new Uri("http://localhost:8000/accounts/login");
+
+			HttpResponseMessage response = await client.GetAsync(uri);
+
+			string responseContent = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(responseContent);
+
+			IEnumerable<Cookie> responseCookies = cookies.GetCookies(uri).Cast<Cookie>();
+			foreach (Cookie cookie in responseCookies)
+			{
+				Console.WriteLine(cookie.Name + ": " + cookie.Value);
+				Console.WriteLine($"domain: {cookie.Domain} | path: {cookie.Path} | expr: {cookie.Expires}");
+			}
+
+			client.Dispose();
+		}
+
 	}
 }
