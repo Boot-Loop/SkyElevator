cac2698db3e9e55f11dd6bb57d9faaacc3f9296a
Azeem Muzammil <azeemmuzammil@yahoo.com>
2020-02-01 01:03:14 +0530

    Handler implemented and tested

--- Core/src/utils/Handler.cs
+++ Core/src/utils/Handler.cs
@@ -9,14 +9,16 @@ using Core.src;
 
 namespace Core.utils
 {
-    public class FileHandler
+    public sealed class FileHandler
     {
 		/* singleton */
 		private FileHandler() { }
-		private static FileHandler singleton;
-		public static FileHandler getSingleton() {
-			if (singleton == null) singleton = new FileHandler();
-			return singleton;
+		private static readonly FileHandler singleton = new FileHandler();
+		public static FileHandler GetFileHandler {
+			get
+			{
+				return singleton;
+			}
 		}
 
 		public static void initialize() { /* TODO: impliment */ }
@@ -24,12 +26,17 @@ namespace Core.utils
 		// System.IO.File.ReadAllText(file_path);
 		
 
-
-
 		/* implement I/O interface for file access.
 		 * sebe client request cache, downloaded respones cache
 		 * also log files -> application, sebe client response logging
 		 */
+
+		/* Copy a File */
+		public void CopyFile(string sourceFileName, string destinationFileName, bool overwrite) {
+
+			File.Copy(sourceFileName, destinationFileName, overwrite);
+			/* System.IO.IOException when rewrite parameter is set to be false is not handled here */
+		}
     }
 	
 	public class DirHandler
@@ -37,11 +44,14 @@ namespace Core.utils
 
 		/* singleton */
 		private DirHandler() { }
-		private static DirHandler singleton;
-		public static DirHandler getSingleton() {
-			if (singleton == null) singleton = new DirHandler();
-			return singleton;
+		private static readonly DirHandler singleton = new DirHandler();
+		public static DirHandler GetDirHandler {
+			get {
+				return singleton;
+			}
 		}
+
+		/* Dir Handler Initialization */
 		public static void initialize() {
 			if (!Directory.Exists(Ref.PROGRAMME_DATA_PATH)) Directory.CreateDirectory(Ref.PROGRAMME_DATA_PATH);
 			if (!Directory.Exists(Ref.LOGS_PATH)) Directory.CreateDirectory(Ref.LOGS_PATH);
@@ -57,10 +67,37 @@ namespace Core.utils
 		 * pc initiaized before each application starts, and if not
 		 * fix any missing dirs
 		 */
-		
 
-		/* create project dir structure */
-		//public static void createProject(String project_name, String project_path );
+
+		/* Create Project Directories */
+
+		public void CreateProject(string projectName, string filePath) {
+
+			string projectDirectory = Path.Combine(filePath, projectName);
+			Directory.CreateDirectory(projectDirectory);
+	
+			Dictionary<string, int> directories = new Dictionary<string, int>() {
+				{ "Inquiry Sheet", 0 },
+				{ "Quotation", 1 },
+				{ "Sales Agreement", 1 },
+				{ "Project Tracking", 0 },
+				{ "Handover", 0 },
+				{ "Maintenance", 0 }
+			};
+			List<string> subDirectories = new List<string>() { 
+				"Client",
+				"Supplier"
+			};
+
+			foreach (KeyValuePair<string, int> directory in directories) {
+				Directory.CreateDirectory(Path.Combine(projectDirectory, directory.Key));
+				if (directory.Value == 1) {
+					foreach (string subDirectory in subDirectories) {
+						Directory.CreateDirectory(Path.Combine(projectDirectory, directory.Key, subDirectory));
+					}
+				}
+			}
+		}
 
 		/* Other implimentations... */
 	}
